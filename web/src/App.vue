f83a0d96474902bc5901f139c1a11a9177a6a7e8
miles <milestdaly@icloud.com>
2021-01-09 11:29:52 -0500

    feat(web): created website

---
+++ web/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div id="chatwindow">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  data(): {
+
+  }
+};
+</script>
+
+<style>
+#chatwindow {
+  background-image: "https://www.transparenttextures.com/patterns/asfalt-dark.png"
+}
+</style>
