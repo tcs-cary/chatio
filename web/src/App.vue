504f0ddc95aef818e8ee66117c9a47e9734b4396
miles <milestdaly@icloud.com>
2021-01-16 12:04:57 -0500

    feat(web): added username chooser

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+    <div v-if="visible" id="usernameField">
+      Choose a username to Begin Chatting: <input type="text" name="" placeholder="Type a username..." value="" v-model="username" >
+      <button type="button" @click="startChatting">Confirm</button>
+    </div>
+    <div v-if="!visible" id="usernameField">
+      <p>Your username is: {{ username }}</p>
+      <button type="button" @click="visible = !visible; username = ''">Change Username</button>
+    </div>
     <div id="chatbox">
       <h1><u>CHAT ROOM</u></h1>
       <div id="chatwindow">
@@ -14,7 +22,18 @@
 export default {
   name: "App",
   data: function() {
-
+    return {
+      username: '',
+      visible: true
+    }
+  },
+  methods: {
+    startChatting: function() {
+      if (this.username === '') {
+        return
+      }
+      this.visible = !this.visible;
+    }
   }
 };
 </script>
@@ -31,11 +50,30 @@ export default {
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 }
 
+#usernameField {
+  text-align: center;
+  margin-top: 15px;
+}
+
 #chatwindow {
   border: 2px solid black;
   margin: 50px 10px 0 10px;
   height: 83%;
   background-color: #a1a1a1;
+}
+
+button {
+  border: 1px black solid;
+  background-color: grey;
+  font-family: 'Yusei Magic', sans-serif;
+}
+
+button:hover {
+  cursor: pointer;
+}
+
+input[type='text'] {
+
 }
 
 html, body {
